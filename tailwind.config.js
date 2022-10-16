bcac6d1ce234d2e7de1aa095b9deb6587b2ea89c
Mohsen jalali <mohsnjli.dev@gmail.com>
2022-10-16 15:35:32 +0330

    primary changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,42 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundColor: {},
-      colors : {},
+
+      backgroundColor: {
+        "primary-green": "#009490",
+        "secondary-black": "#212121",
+      },
+      colors: {
+        "primary-green": "#009490",
+        "secondary-black": "#212121",
+        "therth-gray" : "#B1B1B1",
+        "h1-silver": "#D1D1D1",
+        "black-a5" : "rgba(0 , 0 , 0 ,0.5)",
+      },
+      fontSize: {
+        "small": "12px",
+        "medium": "34px",
+        "large": "50px",
+      },
+      fontWeight: {
+        "regular": 400,
+        "medium": 500,
+        "semiBold": 600,
+        "bold": 700,
+      },
+      width: {
+        "widthMax": "1440"
+      },
+      height: {
+        "nav-height": "60px" , 
+        "500": "500px",
+        "170": "170px"
+      },
+      fontFamily: {
+        "poppins": "'poppins'",
+        "bevan": "'bevan'",
+      }
+
     },
   },
   plugins: [],
